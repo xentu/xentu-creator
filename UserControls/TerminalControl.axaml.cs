a0ecbfc00a6ada969b2667737d613f7752a35704
kodaloid <jakiro.fire@gmail.com>
2022-11-13 23:46:28 +0000

    Restored terminal control as it turns out to be the most reliable way to display console output.

--- UserControls/TerminalControl.axaml.cs
+++ UserControls/TerminalControl.axaml.cs
@@ -4,9 +4,20 @@ namespace XentuCreator.UserControls
 {
     public partial class TerminalControl : UserControl
     {
+        ScrollViewer sv1;
+
+
         public TerminalControl()
         {
             InitializeComponent();
+            sv1 = this.FindControl<ScrollViewer>("scrollViewer1");
+        }
+
+
+
+        public void ScrollToBottom()
+        {
+            sv1.ScrollToEnd();
         }
     }
 }
