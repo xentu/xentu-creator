0df54d1351414c96b475599ccc6a7b91ac9e4203
kodaloid <jakiro.fire@gmail.com>
2022-11-26 22:05:30 +0000

    Added dialog for testing sprite sheet editing.

---
+++ Dialogs/SpriteSheetEditorWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace XentuCreator.Dialogs
+{
+    public partial class SpriteSheetEditorWindow : Window
+    {
+        public SpriteSheetEditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
