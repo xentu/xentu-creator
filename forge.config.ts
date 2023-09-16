3be42c9afbe6853f2beba38587019c4716f5bffe
kodaloid <jakiro.fire@gmail.com>
2023-09-16 23:33:29 +0100

    Some fixes to make publishing work on linux

--- forge.config.ts
+++ forge.config.ts
@@ -1,19 +1,38 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerDeb, MakerDebConfig } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
+const execName = process.platform === "win32" ? 'Xentu Creator' : 'xentu-creator';
+
+
+const debConfig:MakerDebConfig = {
+	options: {
+		section: 'editors',
+		categories: ['Development'],
+		homepage: 'https://xentu.net',
+		icon: 'images/xentu-icon.png'
+	}
+}
+
+
 const config: ForgeConfig = {
 	rebuildConfig: {},
-	makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
 	packagerConfig: {
-		icon: './xentu-icon.ico' // no file extension required
+		executableName: execName,
+		icon: './images/xentu-icon' // no file extension required
 	},
+	makers: [
+		new MakerSquirrel({}),
+		new MakerZIP({},['darwin']),
+		/*new MakerRpm({}), */
+		new MakerDeb(debConfig, ['linux'])
+	],
 	plugins: [
 		new WebpackPlugin({
 			mainConfig,
